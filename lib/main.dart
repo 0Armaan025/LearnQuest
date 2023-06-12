7223d2fb3294e273d713bd0f1fc8af4a6e9b63b8
0Armaan025 <armaan33000@gmail.com>
2023-06-12 11:53:28 +0530

    project config

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:learnquest/screens/onboarding_screen/onboarding_screen.dart';
 
-import 'screens/home_page.dart';
+import 'screens/home_page/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,8 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: OnBoardingScreen(),
     );
   }
 }
-
